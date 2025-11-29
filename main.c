a408d4f8cfab7e06f612f6939deb15049ce9648e
yavidor <yonatan@avidor.org>
2025-11-29 11:13:08 +0200

    Matrix?

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
+#include "main.h"
 #include <stdio.h>
 
 int main(void) {
-    printf("Hello, World!\n");
+    struct Matrix matInstance = {10,10};
+    struct Matrix *mat = &matInstance;
+    mat->m = 10;
+    mat->n = 20;
+    matInstance.m = 15;
+    printf("Matrix(%dx%d)", mat->m, mat->n);
+    printf("Matrix(%dx%d)", matInstance.m, matInstance.n);
     return 0;
 }
---
+++ main.h
@@ -0,0 +1,13 @@
+//
+// Created by yonat on 11/29/2025.
+//
+
+#ifndef MATH_CONCEPTS_MAIN_H
+#define MATH_CONCEPTS_MAIN_H
+
+struct Matrix {
+    int n;
+    int m;
+};
+
+#endif // MATH_CONCEPTS_MAIN_H
